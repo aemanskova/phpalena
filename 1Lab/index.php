e9dc2308327f55d315c33a21843c629b79f3cb61
aemanskova <ae.manskova@mail.ru>
2023-02-16 14:56:43 +0300

    1

---
+++ 1Lab/index.php
@@ -0,0 +1,28 @@
+<html>
+
+<head>
+
+    <title>
+
+        Тестируем PHP
+
+    </title>
+
+</head>
+
+<body>
+    <header>
+        <?php echo "<img src='../1Lab/logo.jpg'>"; ?>
+        <?php echo "1 домашняя работа"; ?>
+    </header>
+    <main>
+        <?php echo 'Привет мир!'; ?>
+    </main>
+    <footer>
+
+    </footer>
+
+
+</body>
+
+</html>
