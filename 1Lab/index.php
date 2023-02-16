103c6bac0a9451cc7b3b3eeac4c0f16ef061e731
aemanskova <ae.manskova@mail.ru>
2023-02-16 15:41:07 +0300

    firstlab commit

--- 1Lab/index.php
+++ 1Lab/index.php
@@ -1,25 +1,32 @@
 <html>
 
 <head>
-
     <title>
-
         Тестируем PHP
-
     </title>
-
+    <?php echo "<link rel='stylesheet' href='../style.css'>"; ?>
 </head>
 
 <body>
     <header>
-        <?php echo "<img src='../1Lab/logo.jpg'>"; ?>
-        <?php echo "1 домашняя работа"; ?>
+        <?php
+        echo "<img width=500 src='../1Lab/logo.jpg'>";
+        echo "<h1 class='text'> Домашняя работа №1 </h1> ";
+        ?>
     </header>
     <main>
-        <?php echo 'Привет мир!'; ?>
+        <?php echo "<p class='text'>Привет мир!</p>" ?>
     </main>
     <footer>
-
+        <?php
+        echo "<table border=1  cellspacing=0 cellpading=0>
+</table>";
+        echo "<table class='table' border=1 cellspacing=0 cellpading=0>
+<tr> <td>Фамилия имя</td> <td>Манскова Алёна</td></tr>
+<tr> <td>Курс университета</td> <td>1</td></tr>
+<tr> <td>Профиль</td> <td>Веб-технологии</td></tr>
+</table>";
+        ?>
     </footer>
 
 
